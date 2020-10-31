62da8c3b688638cf595dd3db86abcd38cd6fcd55
David Arias <davidarias.devs@gmail.com>
2020-10-30 19:03:50 -0500

    :tada: Initial commit: Finishing the course

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <bounce-loader
+            :loading="isLoading"
+            color="#68d391"
+            :size="100"
+            class="m-auto"
+        />
+        <px-assets-table v-if="!isLoading" :assets="assets" />
+    </div>
+</template>
+
+<script>
+import api from "@/api";
+import PxAssetsTable from "@/components/PxAssetsTable.vue";
+
+export default {
+    name: "Home",
+    components: {
+        PxAssetsTable
+    },
+    data() {
+        return {
+            isLoading: true,
+            assets: []
+        };
+    },
+    created() {
+        api.getAssets()
+            .then(assets => (this.assets = assets))
+            .finally(() => (this.isLoading = false));
+    }
+};
+</script>
